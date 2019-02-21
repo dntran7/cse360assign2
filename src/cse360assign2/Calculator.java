f9569fd10bd297fb56a1a18f5b82b1a97532a5d4
dntran7 <dntran7@asu.edu>
2019-02-21 11:37:13 -0700

    initial version

---
+++ src/cse360assign2/Calculator.java
@@ -0,0 +1,82 @@
+/**Name: Diep Tran
+ * Homework Assignment 2
+ * Class ID: 566
+ * Description: performs addition subtraction
+ * division or multiplication and keeps a record of its 
+ * calculations
+*/
+package cse360assign2;
+
+
+/**Class Name: Calculator
+ * Description: Contains a default constructor and 
+ * methods that perform addition subtraction division
+ * or multiplication and keeps a record of its 
+ * calculations
+*/
+public class Calculator {
+
+	private int total;
+	
+	/**Constructor for Calculator class
+	 * Default
+	 * Description: Sets total to 0
+	*/
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**Method getTotal()
+	 * @param none
+	 * @return integer variable total 
+	 * Description: returns total
+	*/
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**Method add()
+	 * @param integer variable "value"
+	 * @return void
+	 * Description: adds value to total
+	*/
+	public void add (int value) {
+		
+	}
+	
+	/**Method subtract()
+	 * @param integer variable "value"
+	 * @return void
+	 * Description: subtracts value from total
+	*/
+	public void subtract (int value) {
+		
+	}
+	
+	/**Method multiply()
+	 * @param integer variable "value"
+	 * @return void
+	 * Description: multiplies value with total
+	*/
+	public void multiply (int value) {
+		
+	}
+	
+	/**Method divide()
+	 * @param integer variable "value"
+	 * @return void
+	 * Description: divides total by value
+	*/
+	public void divide (int value) {
+		
+	}
+	
+	/**Method add()
+	 * @param void
+	 * @return String variable
+	 * Description: returns history log of previous calculations
+	*/
+	public String getHistory () {
+		return "";
+	}
+}
---
+++ src/cse360assign2/maintest.java
@@ -0,0 +1,15 @@
+/**Name: Diep Tran
+ * Homework Assignment 2
+ * Class ID: 566
+ * Description: tests the calculator class
+*/
+
+package cse360assign2;
+
+public class maintest {
+	public static void main (String[] args)
+	{
+		Calculator n = new Calculator();
+	}
+	
+}
